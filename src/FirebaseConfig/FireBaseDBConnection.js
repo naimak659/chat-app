066dd2971f3516352dbd465d10acc0c24e4bb855
naimak659 <naimak659@gmail.com>
2024-08-22 20:10:16 +0600

    don't what is updated

--- src/FirebaseConfig/FireBaseDBConnection.js
+++ src/FirebaseConfig/FireBaseDBConnection.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -14,7 +13,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const appDB = initializeApp(firebaseConfig);
-
-export const FireDb = getFirestore(appDB);
 
 export default appDB;
